f6c2ee0a3db26c42b1e1be52181e19d42af466dd
Divyanshu <agarwaldivyanshu26@gmail.com>
2023-01-22 12:28:31 +0530

    try vercel

--- src/url.js
+++ src/url.js
@@ -1,5 +1,6 @@
 
    let  dev = 'http://localhost:8080/api/v1/';
-    let prod = 'https://stormy-meadow-53146.herokuapp.com/api/v1/';
+    let heroku = 'https://stormy-meadow-53146.herokuapp.com/api/v1/';
+    let vercel = 'https://vercel.com/divyanshu26/library-backend/9q5oe7XRbdSGSGTcSkgJvu5kgiCc';
 
-    module.exports = prod;
+    module.exports = vercel;
