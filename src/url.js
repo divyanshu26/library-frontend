86a725e297d695ad7d776c2c39bae703235cb69a
Divyanshu <agarwaldivyanshu26@gmail.com>
2023-01-22 13:22:15 +0530

    vercel url changes

--- src/url.js
+++ src/url.js
@@ -1,6 +1,6 @@
 
    let  dev = 'http://localhost:8080/api/v1/';
     let heroku = 'https://stormy-meadow-53146.herokuapp.com/api/v1/';
-    let vercel = 'https://vercel.com/divyanshu26/library-backend/9q5oe7XRbdSGSGTcSkgJvu5kgiCc';
+    let vercel = 'library-backend-llhmf8ghl-divyanshu26.vercel.app/api/v1/';
 
     module.exports = vercel;
